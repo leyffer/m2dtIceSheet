7c7a0fdcbba1fc90d042a8d4a6657056e725b54a
nicole <nicole.aretz@t-online.de>
2023-10-31 18:23:37 -0500

    copied over minimum code for drone class, missing gaussian and uniform measurements

--- source/Drone.py
+++ source/Drone.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Drone():
     """!
     This is a general parent class for the drones.
@@ -15,3 +17,30 @@ class Drone():
         self.fom = fom
 
     # TODO: specify and describe other functions the general drone class has to have
+
+    def get_position(self, t: float, alpha: np.ndarray):
+        """! Get the position of the drone given the time and flying parameters
+
+        @param t  The time to evaluate the position of the drone
+        @param alpha  The parameters of the flight path
+        @return  spatial position of the drone
+        """
+        pos, __ = self.get_trajectory(alpha=alpha, grid_t=t * np.ones((1,)))
+        return pos[0, :]
+
+    def get_trajectory(self, alpha: np.ndarray, grid_t: np.ndarray=None) -> tuple[np.ndarray, np.ndarray]:
+        """! Get the trajectory of the drone given the flight parameters alpha
+        @param alpha The specified flight parameters
+        @param grid_t the time grid on which the drone position shall be computed
+        @return  Tuple of (position over flight path, corresponding time for each position)
+        """
+        raise NotImplementedError("Drone.get_trajectory: Needs to be implemented in subclass")
+
+    def measure(self, flightpath, grid_t, state) -> np.ndarray:
+        """! Method to take a measurement
+
+        @param flightpath  The trajectory of the drone
+        @param grid_t  the time discretization on which the flightpath lives
+        @param state  The state which the drone shall measure, State object
+        """
+        raise NotImplementedError("Drone.measure: Needs to be implemented in subclass")
