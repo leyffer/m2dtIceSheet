fixed bug in computation of measurement derivatives

models/AdvectionDiffusion/settings/AdvectionDiffusion_FEniCS/FOM.py
@@ -232,20 +232,20 @@ class FOM(FullOrderModel):
         @return: np.ndarray of dimension (position.shape[0],) containing True at index i if position[i,:]
         is inside the modelled domain
         """
-        n_positions = positions.shape[0]
-        a = (positions >= np.array([0, 0])).all(axis=1)
-        b = (positions <= np.array([1, 1])).all(axis=1)
+        buffer = 0
+        a = (positions >= np.array([0, 0]) + buffer).all(axis=1)
+        b = (positions <= np.array([1, 1]) - buffer).all(axis=1)
         valid_positions = a * b
 
         if self.mesh_shape == "square" or not self.mesh_shape == "houses":
             return valid_positions
 
-        a = (positions > np.array([0.25, 0.15])).all(axis=1)
-        b = (positions < np.array([0.5, 0.4])).all(axis=1)
+        a = (positions > np.array([0.25, 0.15]) - buffer).all(axis=1)
+        b = (positions < np.array([0.5, 0.4]) + buffer).all(axis=1)
         valid_positions[a * b] = False
 
-        a = (positions > np.array([0.6, 0.6])).all(axis=1)
-        b = (positions < np.array([0.75, 0.85])).all(axis=1)
+        a = (positions > np.array([0.6, 0.6]) - buffer).all(axis=1)
+        b = (positions < np.array([0.75, 0.85]) + buffer).all(axis=1)
         valid_positions[a * b] = False
 
         return valid_positions

source/Detector.py
@@ -114,7 +114,7 @@ class Detector:
 
         # initialization
         n_steps = flightpath.shape[0]
-        data = np.NaN * np.ones((n_steps,))
+        data = np.nan * np.ones((n_steps,))
 
         # Loop over the flightpath and measure the state at the positions from
         # the flightpath
@@ -189,14 +189,14 @@ class Detector:
                     pos=flightpath[time_step, :], t=grid_t[time_step], state=state
                 )
 
-        # Stack derivatives next to each other horizontally
-        D_data_d_position = np.hstack(
-            [
-                np.diag(D_data_d_position[:, spatial_dimension])
-                for spatial_dimension in range(n_spatial)
-            ]
-        )
-        return D_data_d_position
+        stacked = np.zeros((grid_t.shape[0], 0))
+        for spatial_dimension in range(n_spatial):
+            block = np.diag(D_data_d_position[:, spatial_dimension])
+            block[~valid_positions, :] = np.nan
+            stacked = np.hstack([stacked, block])
+
+        # return D_data_d_position
+        return stacked
 
     def derivative_at_position(
         self, pos: np.ndarray, t: float, state: "State", **kwargs

source/Noise.py
@@ -138,9 +138,10 @@ class Noise():
         if valid_positions.all():
             return measurement_data.T @ (self.mass_matrix @ measurement_data)
 
+        M = self.restrict_matrix(large_matrix=self.mass_matrix.copy(), valid_positions=valid_positions)
         valid_data = measurement_data[valid_positions]
-        Md = self.mass_matrix[:, valid_positions] @ valid_data
-        return Md.T[:, valid_positions] @ valid_data
+        Md = M @ valid_data
+        return Md.T @ valid_data
 
     def apply_noise_covar_inv(self, measurement_data):
         """! Apply the inverse noise covariance matrix to the observations ` measurement_data`, i.e., compute
@@ -167,9 +168,24 @@ class Noise():
             return Kd
 
         # remove entries
-        covar_inv = LHS[:, valid_positions]
-        covar_inv = covar_inv[valid_positions, :]
+        covar_inv = self.restrict_matrix(large_matrix=LHS, valid_positions=valid_positions)
 
         return covar_inv @ measurement_data[valid_positions]
 
-    # def
+    def restrict_matrix(self, large_matrix, valid_positions):
+
+        invalid_indices = np.where(~valid_positions)[0]
+        if invalid_indices[0] == 0:
+            invalid_indices = invalid_indices[1:]
+
+        large_matrix[invalid_indices - 1, invalid_indices - 1] = large_matrix[0, 0]
+        # todo: make compatible with non-uniform time stepping
+
+        if invalid_indices[-1] < self.n_time_steps - 1:
+            large_matrix[invalid_indices[-1] + 1, invalid_indices[-1] + 1] = large_matrix[0, 0]
+            # todo: make compatible with non-uniform time stepping
+
+        large_matrix = large_matrix[:, valid_positions]
+        large_matrix = large_matrix[valid_positions, :]
+
+        return large_matrix

source/Noise/NoiseBayes.py
@@ -85,6 +85,9 @@ class NoiseBayes(Noise):
         """
         if len(measurement_data.shape) == 1:
             valid_positions = ~np.isnan(measurement_data)
+            measurement_data = np.array([measurement_data]).T
+            if measurement_data.shape[1] != 1:
+                print("wring direction!")
         else:
             valid_positions = ~np.isnan(measurement_data[:, 0])
             # note: since each column of the measurement data is taken for the same flight, the nan entries
@@ -94,7 +97,10 @@ class NoiseBayes(Noise):
             return self.laplacian_matrix @ measurement_data
 
         # remove entries
-        covar_inv = self.laplacian_matrix[:, valid_positions]
-        covar_inv = covar_inv[valid_positions, :]
+        covar_inv = self.restrict_matrix(large_matrix=self.laplacian_matrix.copy(), valid_positions=valid_positions)
 
-        return covar_inv @ measurement_data[valid_positions]
+        action = covar_inv @ measurement_data[valid_positions, :]
+        expanded = np.nan * np.ones(measurement_data.shape)
+        expanded[valid_positions, :] = action
+
+        return expanded